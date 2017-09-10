9ff4f81c7dfc0ebd65ff66932feb07bd29d130de
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2017-09-10 16:39:18 +0200

    Cmake improvements.

--- blitz/bzdebug.h
+++ blitz/bzdebug.h
@@ -177,6 +177,18 @@ _bz_global int  assertSuccessCount BZ_GLOBAL_INIT(0);
         }
 
 
+    #define BZPRECHECK_WITH_LOCATION(X,Y,FILE,LINE)                         \
+        { if (!(X))                                                         \
+          { BZ_STD_SCOPE(cerr) << "[Blitz++] Precondition failure: Module " \
+                               << FILE                                      \
+               << " line " << LINE << BZ_STD_SCOPE(endl)                    \
+               << Y << BZ_STD_SCOPE(endl);                                  \
+            BZ_STD_SCOPE(cerr).flush();                                     \
+            assert(0);                                                      \
+          }                                                                 \
+        }
+
+
     #define BZ_DEBUG_MESSAGE(X)                                    \
         { BZ_STD_SCOPE(cout) << __FILE__ << ":" << __LINE__ << " " \
                              << X << BZ_STD_SCOPE(endl); }
